6cf13649e0499159bdc227b2bc2aec4f49921a25
MD Ziauddin <md.ziauddin02@gmail.com>
2024-01-15 16:06:05 +0530

    Adds Footer: Updates layouts uodate global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,36 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 
+import Header from "@/components/Header/Header";
+import "./globals.css";
+import Footer from "@/components/Footer/Footer";
 
-import Header from '@/components/Header/Header'
-import './globals.css'
-
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '700', '900'], style: ['italic', 'normal'], variable: "--font-poppins" })
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
-  title: 'Hotel Management App',
-  description: 'Discover the best hotel rooms',
-}
+  title: "Hotel Management App",
+  description: "Discover the best hotel rooms",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main className='font-normal'>
+        <main className="font-normal">
           <Header />
           {children}
-          {/* Footer */}
+          <Footer />
         </main>
       </body>
     </html>
-  )
+  );
 }
